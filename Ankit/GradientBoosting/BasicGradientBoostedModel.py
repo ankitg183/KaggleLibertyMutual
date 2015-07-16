790fcf0f8d2ff478d7213cf4cb41c91350fd3012
Ankit <ankig183@gmail.com>
2015-07-16 15:04:16 -0700

    basic GradientBoostedModel

---
+++ Ankit/GradientBoosting/BasicGradientBoostedModel.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+#from sklearn.ensemble import RandomForestRegressor as RF
+from sklearn.ensemble import GradientBoostingRegressor as GR
+
+#read the data
+train = pd.read_csv('../../Common/Data/train.csv')
+test = pd.read_csv('../../Common/Data/test.csv')
+
+#we'll modify the sample submission file to make our submission
+submission = pd.read_csv('../../Common/Data/sample_submission.csv')
+
+#prep the data for sklearn by separating predictors and response
+X = train.drop('Hazard', axis = 1)
+y = train['Hazard']
+
+#one-hot the categoricals
+num_X = pd.get_dummies(X)
+num_Xt = pd.get_dummies(test)
+
+#fit the model and predict
+model = GR().fit(num_X,y)
+prediction = model.predict(num_Xt)
+
+#write the submission file
+submission['Hazard'] = prediction
+submission.to_csv('basic_RF.csv', index = False)
